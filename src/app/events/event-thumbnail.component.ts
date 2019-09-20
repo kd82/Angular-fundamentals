6ba3cbed0aee7c6fccc9bdadabe6a77acff0de15
kd82 <kd82@zips.uakron.edu>
2019-09-20 15:48:12 -0400

    Seperates some code

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,21 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
+import { EventEmitter } from 'events';
 
 @Component({
   selector: 'app-event-thumbnail',
   template: `
     <div class="well hoverwell thumbnail">
-      <h2>{{ event.name }}</h2>
-      <div>Date: {{ event.date }}</div>
-      <div>Time: {{ event.time }}</div>
-      <div>Price: \${{ event.price }}</div>
+      <h2>{{ eventdata.name }}</h2>
+      <div>Date: {{ eventdata.date }}</div>
+      <div>Time: {{ eventdata.time }}</div>
+      <div>Price: \${{ eventdata.price }}</div>
       <div>
-        <span>Location: {{ event.location.address }}</span>
+        <span>Location: {{ eventdata.location.address }}</span>
         <span>&nbsp;</span>
-        <span>{{ event.location.city }}, {{ event.location.country }}</span>
+        <span>{{ eventdata.location.city }}, {{ eventdata.location.country }}</span>
       </div>
+      <button class="btn btn-primary" (click)="handleClickMe()">
+        Click me!
+      </button>
     </div>
   `
 })
 export class EventsThumbnailComponent {
-  @Input() event: any;
+  @Input() eventdata: any;
 }
