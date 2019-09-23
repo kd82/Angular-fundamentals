e1a1786001306f003c22f501e0ff619399e6a38b
kd82 <kd82@zips.uakron.edu>
2019-09-23 12:28:58 -0400

    Uses ngIf and ngFor

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -4,24 +4,34 @@ import { Component, Input } from '@angular/core';
   selector: 'app-event-thumbnail',
   template: `
     <div class="well hoverwell thumbnail">
-      <h2>{{ eventdata.name }}</h2>
-      <div>Date: {{ eventdata.date }}</div>
-      <div>Time: {{ eventdata.time }}</div>
-      <div>Price: \${{ eventdata.price }}</div>
-      <div>
-        <span>Location: {{ eventdata.location.address }}</span>
+      <h2>{{ eventdata?.name }}</h2>
+      <div>Date: {{ eventdata?.date }}</div>
+      <div>Time: {{ eventdata?.time }}</div>
+      <div>Price: \${{ eventdata?.price }}</div>
+      <div *ngIf="eventdata?.location">
+        <span>Location: {{ eventdata?.location?.address }}</span>
         <span class="pad-left"></span>
         <span
-          >{{ eventdata.location.city }}, {{ eventdata.location.country }}</span
+          >{{ eventdata?.location?.city }},
+          {{ eventdata?.location?.country }}</span
         >
+      </div>
+      <div *ngIf="eventdata?.onlineUrl">
+      Online URL : {{eventdata?.onlineUrl}}
       </div>
     </div>
   `,
   styles: [
     `
-      .pad-left {margin-left: 10px;}
-      .well div {color:#bbb;}
-      .thumbnail {min-height:210px;}
+      .pad-left {
+        margin-left: 10px;
+      }
+      .well div {
+        color: #bbb;
+      }
+      .thumbnail {
+        min-height: 210px;
+      }
     `
   ]
 })
